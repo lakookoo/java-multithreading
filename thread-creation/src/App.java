4f1207d69cc848500c0c5e6b8f1cf95aa4a5cf17
Larisa <kosareva.larisa@gmail.com>
2024-05-17 19:09:33 +0200

    Create a new thread extending Thread class

--- thread-creation/src/App.java
+++ thread-creation/src/App.java
@@ -1,27 +1,17 @@
 public class App {
     public static void main(String[] args) throws InterruptedException {
-       Thread thread = new Thread(new Runnable() {
+       Thread thread = new NewThread();
+       
+       thread.start();
+    }
+
+    private static class NewThread extends Thread {
         @Override
         public void run() {
             // code that will run in a new thread
-            throw new RuntimeException("Intentional exception");
+            
+            System.out.println(" hello from " + Thread.currentThread().getName());
             
         }
-       });
-       thread.setName("Misbehaving Thread");
-
-        
-       thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-        @Override
-        public void uncaughtException(Thread t, Throwable e) {
-            System.out.println("A critical error happened in a thread " + t.getName() + " the error is " + e.getMessage() );
-            
-        }
-       });
-       thread.start();
-      
-
-       
-
     }
 }
