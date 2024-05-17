96aa38a494a5436f8c7463b811e5eccf097f8e49
Larisa <kosareva.larisa@gmail.com>
2024-05-17 19:04:22 +0200

    Set uncaught exception to a thread

--- thread-creation/src/App.java
+++ thread-creation/src/App.java
@@ -4,18 +4,22 @@ public class App {
         @Override
         public void run() {
             // code that will run in a new thread
-            System.out.println("We are now in thread: " + Thread.currentThread().getName());
-            System.out.println("Current thread priority is : " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Intentional exception");
             
         }
        });
-       thread.setName("New Worker Thread");
-       thread.setPriority(Thread.MAX_PRIORITY);
-       System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
-       thread.start();
-       System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+       thread.setName("Misbehaving Thread");
 
-       Thread.sleep(1000);// it will not consume any CPU resources
+        
+       thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            System.out.println("A critical error happened in a thread " + t.getName() + " the error is " + e.getMessage() );
+            
+        }
+       });
+       thread.start();
+      
 
        
 
