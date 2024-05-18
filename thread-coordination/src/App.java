a73efaf876bcadc8aafff9431a0ee7cd746745c1
Larisa <kosareva.larisa@gmail.com>
2024-05-18 18:24:03 +0200

    prematurely interrupted thread

--- thread-coordination/src/App.java
+++ thread-coordination/src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Thread thread = new Thread(new LongComputationTask(new BigInteger("20000000"), new BigInteger("100000000")));
         thread.start();
+        thread.interrupt();
         
     }
 
@@ -24,6 +25,10 @@ public class App {
     private static BigInteger pow(BigInteger base, BigInteger power){
         BigInteger result = BigInteger.ONE;
         for(BigInteger i = BigInteger.ONE; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)){
+            if(Thread.currentThread().isInterrupted()){
+                System.out.println("Prematurely interrupted computation");
+                return BigInteger.ZERO;
+            }
             result = result.multiply(base);
 
         }
