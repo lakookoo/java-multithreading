251f6c04b97437646cc37db1d335ba1382a75b52
Larisa <kosareva.larisa@gmail.com>
2024-05-18 18:21:25 +0200

    long computation task

--- thread-coordination/src/App.java
+++ thread-coordination/src/App.java
@@ -1,18 +1,32 @@
+import java.math.BigInteger;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Thread thread = new Thread(new BlockingTask());
+        Thread thread = new Thread(new LongComputationTask(new BigInteger("20000000"), new BigInteger("100000000")));
         thread.start();
-        thread.interrupt();
+        
     }
 
-    private static class BlockingTask implements Runnable {
+    private static class LongComputationTask implements Runnable {
+        private BigInteger base;
+        private BigInteger power;
+
+        public LongComputationTask(BigInteger base, BigInteger power){
+            this.base = base;
+            this.power = power;
+        }
         @Override
         public void run() {
-            try {
-                Thread.sleep(5000000);
-            } catch (Exception e) {
-                System.out.println("Exiting blocking thread");
-            }
+            System.out.println(base + "^" + power + " = " + pow(base, power));
         }
+    }
+
+    private static BigInteger pow(BigInteger base, BigInteger power){
+        BigInteger result = BigInteger.ONE;
+        for(BigInteger i = BigInteger.ONE; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)){
+            result = result.multiply(base);
+
+        }
+        return result;
     }
 }
